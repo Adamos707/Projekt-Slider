525d0a2b38756c0aae65209ed2080568a1c942ff
Adamos707 <79307117+Adamos707@users.noreply.github.com>
2024-01-10 14:53:50 +0100

    Add files via upload

---
+++ js/refresh.js
@@ -0,0 +1,6 @@
+function refreshe (){
+    if(document.URL==="file:///C:/Users/Ucze%C5%84/Documents/Adam%20Watkowski%201P/Projekt2/index.html#HOME" || "file:///C:/Users/Ucze%C5%84/Documents/Adam%20Watkowski%201P/Projekt2/index.html#GALLERY" || "file:///C:/Users/Ucze%C5%84/Documents/Adam%20Watkowski%201P/Projekt2/index.html#EFFECTS"){
+        window.location.replace("file:///C:/Users/Ucze%C5%84/Documents/Adam%20Watkowski%201P/Projekt2/index.html")
+    }
+    
+}
